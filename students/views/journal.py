400e902fb6d874f53352167e21b77a67b7b6a984
Natalya Pimankova <natalya2487@gmail.com>
2016-07-13 09:31:59 +0300

    journal edit thead

--- students/views/journal.py
+++ students/views/journal.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime, date
 from dateutil.relativedelta import relativedelta
+from calendar import monthrange, weekday, day_abbr
 
 # Views for Journal
 def journal(request):
@@ -18,6 +19,12 @@ def journal(request):
     context['year'] = month.year
     context['next_month'] = next_month.strftime('%Y-%m-%d')
     context['prev_month'] = prev_month.strftime('%Y-%m-%d')
+
+    myear, mmonth = month.year, month.month
+    number_of_days = monthrange(myear, mmonth)[1]
+    context['month_header'] = [{'day': d,
+                                'verbose': day_abbr[weekday(myear, mmonth, d)][:2]}
+                               for d in range(1, number_of_days + 1)]
 
     journal = (
         {
