8c8484dbfc17ba5b5eba65fc9b563c0f6d1707ac
Natalya Pimankova <natalya2487@gmail.com>
2016-08-05 10:10:29 +0300

    change models for meta and unicode

--- students/models.py
+++ students/models.py
@@ -7,6 +7,10 @@ from django.db import models
 class Student(models.Model):
     """Student Model"""
 
+    class Meta(object):
+        verbose_name = u"Студент"
+        verbose_name_plural = u"Студенти"
+
     first_name = models.CharField(
         max_length=256,
         blank=False,
@@ -26,7 +30,7 @@ class Student(models.Model):
         default=''
     )
 
-    birstday = models.DateField(
+    birthday = models.DateField(
         blank=False,
         verbose_name=u"Дата народження",
         null=True
@@ -48,3 +52,6 @@ class Student(models.Model):
         blank=True,
         verbose_name=u"Додаткові нотатки"
     )
+
+    def __unicode__(self):
+        return u"%s %s" % (self.first_name, self.last_name)
